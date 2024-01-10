d3c7b5943829a7e94ed291fb2ca9d3c417b384cb
Saideep Goud <saideep2244@gmail.com>
2024-01-11 00:31:39 +0530

    slight change in margin

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -18,7 +18,7 @@ const VideoContainer = () => {
   if (ytVideos === null) return <Shimmer />;
   else
     return (
-      <div className="mt-10 pt-4 ml-4  grid grid-cols-3 gap-3">
+      <div className="mt-10 pt-4 ml-4  grid grid-cols-3 gap-x-1 gap-y-8">
          {ytVideos.map((video) => (
           <VideoCard key={video.id} info={video} />
         ))} 
